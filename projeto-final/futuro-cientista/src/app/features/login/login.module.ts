3707bc55a02ba667cb424a2fa6cdc37eff0a11c7
lucaslabre <lucaslabre@gmail.com>
2021-07-31 12:43:02 -0300

    organizing register page

--- projeto-final/futuro-cientista/src/app/features/login/login.module.ts
+++ projeto-final/futuro-cientista/src/app/features/login/login.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { LoginUserPageComponent } from './pages/login-user-page/login-user-page.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { RegisterComponent } from './components/register/register.component';
 import { RegisterUserPageComponent } from './pages/register-user-page/register-user-page.component';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
@@ -13,7 +12,6 @@ import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     LoginUserPageComponent,
-    RegisterComponent,
     RegisterUserPageComponent
   ],
   imports: [
--- projeto-final/futuro-cientista/src/app/features/login/pages/register-user-page/register-user-page.component.ts
+++ projeto-final/futuro-cientista/src/app/features/login/pages/register-user-page/register-user-page.component.ts
@@ -1,10 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { faFacebookF, faGooglePlusG, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope, faLock, faUser } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   templateUrl: './register-user-page.component.html',
   styleUrls: ['./register-user-page.component.scss']
 })
 export class RegisterUserPageComponent implements OnInit {
+
+  faFacebookF = faFacebookF;
+  faGooglePlusG = faGooglePlusG;
+  faLinkedinIn = faLinkedinIn;
+  faUser = faUser;
+  faEnvelope = faEnvelope;
+  faLock = faLock;
 
   constructor() { }
 
